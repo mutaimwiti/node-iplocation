aa1a13639488b040f7dbff19cd030773767db1b7
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-05-22 20:05:38 +0300

    Add messages to direct user action

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
+const iplocate = require("node-iplocate");
 const express = require('express');
 
 const app = express();
 
-app.get('/:ipAddress', (req, res) => {
-  const {params: {ipAddress}} = req;
+app.get('/', (req, res) => {
+  const appUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-  const iplocate = require("node-iplocate");
-
-  iplocate(ipAddress).then(function (results) {
-    return res.json({
-      location: results
-    })
-  });
+  return res.status(422).json({
+    message: `Supply the IP address: ${appUrl}ipAddress`,
+    example: `${appUrl}8.8.8.8`
+  })
 });
 
-app.listen(3000, () => {
-  console.log("App is listening at http://localhost:3000");
+app.get('/:ipAddress', async (req, res, next) => {
+  const {params: {ipAddress}} = req;
+
+  if (ipAddress) {
+    try {
+      const results = await iplocate(ipAddress);
+
+      if (results) {
+        return res.json({
+          location: results
+        })
+      }
+    } catch (e) {}
+  }
+
+  return res.status(500).json({
+    message: "Could not get your location. Ensure the IP address is valid and try again"
+  })
+});
+
+app.listen(3001, () => {
+  console.log("App is listening at http://localhost:3001");
 });
